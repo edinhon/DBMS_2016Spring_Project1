7db4ea6e5a0984fc2229d60ad1cfe4bec8e98b0e
billywithbelly <billychen1216@gmail.com>
2017-04-09 20:38:06 +0800

    optimize 'where' of select

--- SelectInst.h
+++ SelectInst.h
@@ -16,37 +16,37 @@ class SelectInst : public Instruction {
 		SelectInst(string const name);
 		~SelectInst(){};
 		
+		// from
 		vector<string> tableNames;	//FROM table name
 									//檢查是否重複	
-		vector<bool> isTableNameAlias;	//是否有alias name							
-									
+		////
+		vector<bool> isTableNameAlias;	//是否有alias name														
+		///			
 		vector<string> tableNameAlias;	//Table Name alias
-		
+		////
 		vector<int> tableNameAliasIndex;	//紀錄alias name對應哪個table name
 		
+		// select
 		vector<string> selectedAttributesNames;	//SELECT 的 attribute name
-		
+		////
 		vector<bool> isSelectedAttributesTables;//attribute是否有table name, ex: t.attribute
 		
 		vector<string> selectedAttributesTables;//attribute 的 table name
-		
+		////
 		vector<int> selectedAttributesTablesIndex;	//紀錄attribute table name對應哪個attribute
 		
-		bool isSelectAllAttrs;	//是否(*)
+		bool isSelectAllAttrs[2];	//是否(*)
 		
 		bool isWHERE;	//是否有WHERE
 		
+		// where
 		vector<string> WHERE_FirstAttrNames;	//WHERE的first attributes or values, ex: WHERE t."attr" = t.attr
-		
-		vector<string> WHERE_FirstIsTables;	//WHERE的first attributes or values是否有table name
 		
 		vector<string> WHERE_FirstAttrTables;	//WHERE的first table names, ex: WHERE "t".attr = t.attr
 		
 		vector<int> WHERE_FirstTypes;	//WHERE clause value type, 0 = int, 1 = varchar, 2 = attribute, -1 = error
 		
 		vector<string> WHERE_SecondAttrNames;	//WHERE的first attributes or values, ex: WHERE t.attr = t."attr"
-		
-		vector<string> WHERE_SecondtIsTables;	//WHERE的first attributes or values是否有table name
 		
 		vector<string> WHERE_SecondAttrTables;	//WHERE的first table names, ex: WHERE t.attr = "t".attr
 		
@@ -58,13 +58,7 @@ class SelectInst : public Instruction {
 		
 		bool isCOUNT;
 		
-		vector<string> COUNT_Attributes;
-		
-		bool isCOUNTAllAttrs;	//total number of WHERE clause
-		
 		bool isSUM;
-		
-		vector<string> SUM_Attributes;
 		
 };
 
